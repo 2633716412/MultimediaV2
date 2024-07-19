374d79e0aef0da18f19b2828a4fcab2b2dc2e11f
liqi <2633716412@qq.com>
2024-07-19 15:07:52 +0800

    呼叫等待

--- app/src/test/java/com/example/multimediav2/ExampleUnitTest.java
+++ app/src/test/java/com/example/multimediav2/ExampleUnitTest.java
@@ -1,8 +1,16 @@
 package com.example.multimediav2;
 
+import com.example.multimediav2.Utils.NetWorkUtils;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.TimerTask;
+
+import Modules.EDate;
+import Modules.LogHelper;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +20,58 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+        /*EDate now = EDate.Now();
+        EDate secondDay=now.AddDays(1);
+        String yearStr =now.ToString();
+        int year = Integer.parseInt(yearStr.substring(0,4));
+        System.out.println(secondDay.Year()+"-"+secondDay.Month()+"-"+secondDay.Day());
+        System.out.println(year+"-"+secondDay.Month()+"-"+secondDay.Day());*/
+        EDate now = EDate.Now();
+        now=now.AddDays(1);
+        EDate begin = new EDate(now.Year(), now.Month(), now.Day(), now.date.getHours(), now.date.getMinutes(), 0);
+        System.out.println(begin.ToString());
+        /*try {
+            String gbIp= NetWorkUtils.GetGBIp();
+            System.out.println("发送地址: " + gbIp);
+            DatagramSocket socket = new DatagramSocket();
+            socket.setBroadcast(true);
+            String message = "你的广播消息";
+            byte[] sendData = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(gbIp), 8888);
+            socket.send(packet);
+            System.out.println("发送成功");
+
+            socket.close();
+        } catch (Exception e) {
+            LogHelper.Error("组播发送异常："+e.toString());
+        }*/
+    }
+    public static String filterSpecialChars(String str) {
+        if (str == null || str.trim().isEmpty()) {
+            return str;
+        }
+        String pattern = "[^a-zA-Z0-9\\u4E00-\\u9FA5\\s\\[\\]\\{\\}\\(\\),\\\"'?=_:./\\\\-]"; // 只允许字母、数字和中文
+        return str.replaceAll(pattern, "");
+    }
+
+    static class SenderTask extends TimerTask {
+            @Override
+            public void run() {
+                try {
+                    String gbIp= NetWorkUtils.GetGBIp();
+                    System.out.println("发送地址: " + gbIp);
+                    DatagramSocket socket = new DatagramSocket();
+                    socket.setBroadcast(true);
+                    String message = "你的广播消息";
+                    byte[] sendData = message.getBytes();
+                    DatagramPacket packet = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(gbIp), 8888);
+                    socket.send(packet);
+                    System.out.println("发送成功");
+                    socket.close();
+                } catch (Exception e) {
+                    LogHelper.Error("组播发送异常："+e.toString());
+                }
+            }
     }
 }
